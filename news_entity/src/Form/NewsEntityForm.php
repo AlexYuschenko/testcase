0bf1c71b0aa60f80e9e02d71a7a79976090a74dc
Oleksandr Yushchenko <OYushche@its.jnj.com>
2019-07-19 16:43:31 +0300

    Multistep functionality for News entity creation

--- news_entity/src/Form/NewsEntityForm.php
+++ news_entity/src/Form/NewsEntityForm.php
@@ -4,6 +4,7 @@ namespace Drupal\news_entity\Form;
 
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -23,6 +24,20 @@ class NewsEntityForm extends ContentEntityForm {
    * @var \Drupal\Core\Session\AccountProxyInterface
    */
   protected $account;
+
+  /**
+   * Multi steps of the form.
+   *
+   * @var int
+   */
+  protected $step;
+
+  /**
+   * Boolean indicating whether the entity form is multistep.
+   *
+   * @var bool
+   */
+  protected $multistep;
 
   /**
    * Constructs a new NewsEntityForm.
@@ -60,32 +75,83 @@ class NewsEntityForm extends ContentEntityForm {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     /* @var \Drupal\news_entity\Entity\NewsEntity $entity */
-    $form = parent::buildForm($form, $form_state);
+    if ($this->entity->isNew()) {
+      if (!$form_state->has('entity_form_initialized')) {
+        $this->init($form_state);
+      }
 
-    return $form;
+      $this->step = $form_state->get('page_num') ?: 1;
+      $this->multistep = FALSE;
+
+      $form_display = 'news_entity.news_entity.step_' . $this->step;
+      if ($entity_form_display = EntityFormDisplay::load($form_display)) {
+        $this->multistep = TRUE;
+        $this->setFormDisplay($entity_form_display, $form_state);
+      }
+
+      $form = parent::buildForm($form, $form_state);
+
+      if ($this->multistep) {
+        if ($this->step == 1) {
+          $form_state->set('page_num', 1);
+          $form['actions']['submit']['#value'] = $this->t('Next');
+        }
+        else {
+          $form['actions']['back'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Back'),
+            '#submit' => ['::pageBackSubmit'],
+            '#limit_validation_errors' => [],
+          ];
+        }
+      }
+
+      return $form;
+    }
+
+    return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state) {
-    $entity = $this->entity;
-
-    $status = parent::save($form, $form_state);
-
-    switch ($status) {
-      case SAVED_NEW:
-        $this->messenger()->addMessage($this->t('Created the %label News.', [
-          '%label' => $entity->label(),
-        ]));
-        break;
-
-      default:
-        $this->messenger()->addMessage($this->t('Saved the %label News.', [
-          '%label' => $entity->label(),
-        ]));
+    if ($form_state->has('page_num') && $form_state->get('page_num') == 1) {
+      $form_state->set('page_num', 2)->setRebuild(TRUE);
     }
-    $form_state->setRedirect('entity.news_entity.canonical', ['news_entity' => $entity->id()]);
+    else {
+      $entity = $this->entity;
+
+      $status = parent::save($form, $form_state);
+
+      switch ($status) {
+        case SAVED_NEW:
+          $this->messenger()->addMessage($this->t('Created the %label News.', [
+            '%label' => $entity->label(),
+          ]));
+          break;
+
+        default:
+          $this->messenger()->addMessage($this->t('Saved the %label News.', [
+            '%label' => $entity->label(),
+          ]));
+      }
+      $form_state->setRedirect('entity.news_entity.canonical', ['news_entity' => $entity->id()]);
+    }
+  }
+
+  /**
+   * Provides custom submission handler for 'Back' button (page 2).
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function pageBackSubmit(array &$form, FormStateInterface $form_state) {
+    $form_state
+      ->set('page_num', 1)
+      ->setRebuild(TRUE);
   }
 
 }
